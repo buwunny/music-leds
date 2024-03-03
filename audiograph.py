237511f5afae78752cf19e1f9efb3e85eeb2dd24
Bunny <97325724+buwunny@users.noreply.github.com>
2024-03-03 12:57:01 -0500

    Add files via upload

---
+++ audiograph.py
@@ -0,0 +1,115 @@
+import pyaudio
+import numpy as np
+import matplotlib.pyplot as plt
+import serial
+import time
+import threading
+
+arduino = serial.Serial('COM5', 115200 , timeout=0.1) #rtscts=True)
+def to_arduino(x):
+    arduino.write(bytes(x, 'utf-8'))
+    # time.sleep(0.05)
+    data = arduino.readline().decode('utf-8')
+    # print(data)
+    # if x in data:
+    return data
+    # else:
+    #     to_arduino(x)
+
+# Set the audio parameters
+CHUNK = 1024  # Number of frames per buffer
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100 
+
+# Initialize PyAudio
+p = pyaudio.PyAudio()
+
+# Open the microphone stream
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+# Create a figure and axis for plotting
+fig, ax = plt.subplots()
+
+# Create a time axis for the x-axis
+x = np.arange(0, 2 * CHUNK, 2)
+
+# Create an empty line object for the plot
+line, = ax.plot(x, np.random.rand(CHUNK), '-')
+
+# Set the y-axis limits
+# ax.set_ylim(-32768, 32767)
+ax.set_ylim(-5000, 5000)
+
+def update_plot(stream):
+    while True:
+        data = stream.read(CHUNK)
+
+        # Convert the data to a numpy array
+        audio_data = np.frombuffer(data, dtype=np.int16)
+        # Update the line object with the new audio data
+        line.set_ydata(audio_data)
+        
+        # Redraw the plot
+        fig.canvas.draw()
+
+        # Flush any pending GUI events
+        plt.pause(0.01)
+def update_arduino(stream):
+    while True:
+        data = stream.read(CHUNK)
+        audio_data = np.frombuffer(data, dtype=np.int16)
+        intensity = np.max(audio_data)
+
+        # Apply the FFT to the audio data
+        # fft_data = np.fft.fft(audio_data)
+
+        # Calculate the frequencies corresponding to the FFT bins
+        # frequencies = np.fft.fftfreq(len(fft_data), 1/RATE)
+
+        # # Find the index of the maximum magnitude in the FFT data
+        # max_index = np.argmax(np.abs(fft_data))
+
+        # # Get the frequency corresponding to the maximum magnitude
+        # max_frequency = int(abs(frequencies[max_index]))
+
+        to_arduino(str(intensity))# + "|" + str(max_frequency))
+        # Print the calculated frequency
+        # print("Frequency:", max_frequency)
+        # print("REVERSE" + str(max_frequency)[::-1])
+        # time.sleep(0.01)
+        # print(stream.get_time()
+
+
+thread = threading.Thread(target=update_arduino, args=(stream,))
+thread.start()
+
+update_plot(stream)
+# while True:
+#     # Read audio data from the stream
+#     data = stream.read(CHUNK)
+
+#     # Convert the data to a numpy array
+#     audio_data = np.frombuffer(data, dtype=np.int16)
+
+#     # pusdeo_freq = np.zeros(audio_data==0)
+#     psudeo_intn = np.max(audio_data)
+#     # print(psudeo_intn)
+#     to_arduino(str(psudeo_intn))
+#     # # Update the line object with the new audio data
+#     line.set_ydata(audio_data)
+    
+#     # # Redraw the plot
+#     fig.canvas.draw()
+    
+
+#     # # Flush any pending GUI events
+#     plt.pause(0.01)
+# Close the stream and terminate PyAudio
+stream.stop_stream()
+stream.close()
+p.terminate()
---
+++ sketch_feb23b.ino
@@ -0,0 +1,75 @@
+#include <LiquidCrystal.h>
+#include <FastLED.h>
+
+#define NUM_LEDS 20
+#define DATA_PIN 6
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+int piezo = A0;
+// int past = 0;
+
+CRGB color = CRGB::Red;
+
+CRGB leds[NUM_LEDS];
+CRGB temp[NUM_LEDS];
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setTimeout(1);
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);  // GRB ordering is assumed
+ 
+  lcd.print("Piezo:");
+  lcd.setCursor(0, 1);
+  lcd.print(analogRead(piezo));
+}
+
+void loop() {
+  while (!Serial.available());
+  float data = Serial.readString().toInt();
+  // int intensity;
+  // int frequency;
+  // for (int i = 0; i < data.length(); i++){
+  //   int mid = data.indexOf("|");
+  //   intensity = data.substring(0, mid-1).toInt();
+  //   frequency = data.substring(mid, data.length()).toInt();
+
+  // }
+  
+  Serial.println(data);
+  // Serial.print("intensity" + intensity);
+  // Serial.print(" frequency" + frequency);
+  // int a = current/100;
+  // int b =  current % 10 * 10 + current % 100;
+
+  // if (b > 5) {
+  //   color = CRGB::Red;
+  // }
+  // else{
+  //   color = CRGB::Blue;
+  // }
+  
+  for (int i = 0; i <= NUM_LEDS; i++) {
+        // Serial.print(i);
+    if (i <= data / 3000 * 10) {
+      leds[i] = CRGB::Red;
+      // if (i % 2 == 0 ) {
+      //   i = 19 - i;
+      // }
+
+      // if (i > NUM_LEDS/2 - 1){
+      //   i = map(i, 10, 20, 20, 10)
+      //   leds[i] = CRGB::Red;
+      // }
+      
+    } else {
+      leds[i] = CRGB::Black;
+    }
+  }
+  FastLED.show();
+
+
+  // leds[past] = CRGB::HTMLColorCode;
+  // past = current;
+  // delay(50);
+}
